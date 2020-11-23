8c1c51eab110c319e033a2ce64ebdf953aeb0727
wilsonng21 <wilsongch92@gmail.com>
2020-11-23 17:44:07 -0500

    changed movie api

--- src/App.js
+++ src/App.js
@@ -8,27 +8,28 @@ import MovieList from './components/MovieList'
 import Movie from './components/Movie'
 
 function App() {
-
+  const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
   const [data, setData] = useState({})
   const [query, setQuery] = useState('')
 
   const doSearch = input => setQuery(input)
 
-  const searchMovies = query => {
-      axios.get('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup', {
-        params: {term: query},
-        headers: {
-          'x-rapidapi-key': process.env.REACT_APP_MOVIE_API_KEY,
-          'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-        }
-      })
-      .then(response => setData(response.data))
-      .catch(error => console.error('Error fetching and parsing data', error))
+  const searchMovies = async(query) => {
+    if(query){
+      try {
+        let response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`)
+        console.log(response.data.results)
+      } catch(error) {
+        console.error('Error fetching and parsing data', error)
+      }
+      // .then(response => console.log(response))
+      // .catch(error => console.error('Error fetching and parsing data', error))
+    }
   }
 
   useEffect(() => {
     searchMovies(query)
-  }, [query])
+  }, [searchMovies])
 
   const singleMovie = (routerProps) => {
     let movie_id = routerProps.match.params.id
